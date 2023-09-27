262a4fe94b846fd092bd767f81334df78740d03d
Priyanshu Raj Singh <72143359+priyanshu24071@users.noreply.github.com>
2023-09-28 02:46:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,77 +1,49 @@
-import csv
-import nltk
-import numpy as np
-from nltk.tokenize import word_tokenize
-from flask import Flask, request, render_template, jsonify, session
-from nltk.corpus import stopwords
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-
+from flask import Flask, render_template, request
+from llama_index import SimpleDirectoryReader, LlamaListIndex, LlamaSimpleVectorIndex, LLMPredictor, PromptHelper
+from langchain.chat_models import ChatOpenAI
 app = Flask(__name__)
 
-qa_pairs = {}
-with open('combined.csv', 'r', encoding='utf-8') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
-    for row in csv_reader:
-        question = row['question']
-        answer = row['answer']
-        qa_pairs[question] = answer
+# Define the index and chatbot logic
+class IndexAndChatbot:
+    def __init__(self, directory_path):
+        self.directory_path = directory_path
+        self.index = self.construct_index()
+    
+    def construct_index(self):
+        max_input_size = 4096
+        num_outputs = 512
+        max_chunk_overlap = 20
+        chunk_size_limit = 600
 
-nltk.download('punkt')
-nltk.download('stopwords')
-stop_words = set(stopwords.words('english'))
+        prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
 
-questions = list(qa_pairs.keys())
-tfidf_vectorizer = TfidfVectorizer(tokenizer=word_tokenize, stop_words=stop_words)
-tfidf_matrix = tfidf_vectorizer.fit_transform(questions)
-cosine_similarities = cosine_similarity(tfidf_matrix, tfidf_matrix)
+        llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.1, model_name="gpt-3.5-turbo", max_tokens=num_outputs))
 
+        documents = SimpleDirectoryReader(self.directory_path).load_data()
 
-def get_most_similar_question(user_input):
-    input_vector = tfidf_vectorizer.transform([user_input])
-    similarities = cosine_similarity(input_vector, tfidf_matrix)[0]
-    most_similar_idx = np.argmax(similarities)
-    return questions[most_similar_idx]
+        index = GPTVectorStoreIndex(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
 
+        index.save_to_disk('index.json')
 
-def contains_name(input_text, name):
-    return name.lower() in input_text.lower()
+        return index
 
+    def chatbot(self, input_text):
+        input_text += "Don't provide information outside the given context."
+        response = self.index.query(input_text, response_mode="default")
+        return response.response
+
+# Initialize your chatbot with the data directory path
+chatbot_instance = IndexAndChatbot("data")
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
-@app.route('/get_response', methods=['POST'])
-def get_response():
-    response="Hello there! Welcome to the chatbot. Please provide your role (e.g., Doctor, Nurse, Health Care Assistant)."
-    user_input = request.form['user_input']
-
-    if "role" not in session:
-        session["role"] = user_input
-        response = "Please provide your department(e.g., Inpatient, Outpatient, Maternity or Emergency.)"
-        
-
-    elif "department" not in session:
-        session["department"] = user_input
-        response = "Thank you for specifying your department. How can I assist you today?"
-    else:
-        if user_input.lower() == 'bye' or user_input.lower() == 'exit' or user_input.lower() == 'quit':
-            response = "Bye! See you soon!"
-        if user_input.lower() =='hi' or user_input.lower() =='hello' or user_input.lower()=='hey' or user_input.lower()=='greetings':
-            response='Hello! How may I help you?'
-        elif user_input in qa_pairs:
-            response = qa_pairs[user_input]
-        else:
-            most_similar_question = get_most_similar_question(user_input)
-            response = qa_pairs[most_similar_question]
-            if most_similar_question.lower() != user_input.lower():
-                response += " (Did you mean: '{}')".format(most_similar_question)
-    
-    return jsonify({'response': response})
-
+@app.route('/chat', methods=['POST'])
+def chat():
+    input_text = request.form['user_input']
+    response = chatbot_instance.chatbot(input_text)
+    return render_template('index.html', user_input=input_text, chatbot_response=response)
 
 if __name__ == '__main__':
-    app.secret_key = 'ABCD1234'
     app.run(debug=True)
